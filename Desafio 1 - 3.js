5b9aa9dcf9606acbc9493a7dcfb7e80fa530ef52
RafaelDalarosa <rafaelhcd@hotmail.com>
2020-01-28 20:23:13 -0300

    atualizacao do codigo

---
+++ Desafio 1 - 3.js
@@ -0,0 +1,20 @@
+const usuarios = [{
+    nome: 'Carlos',
+    tecnologias: ['HTML', 'CSS']   
+},
+{
+    nome: 'Jasmine',
+    tecnologias: ['JavaScript', 'CSS']
+},
+{
+    nome: 'Tuane',
+    tecnologias: ['HTML', 'Node.js']
+}]
+
+
+function buscaTech(tech){
+   for (let i = 0; i < tech.length; i++) 
+        console.log(i)
+   }
+}
+
