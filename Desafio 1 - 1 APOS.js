451ffc8f319b73aecfe649b96e7c76d764946c22
RafaelDalarosa <rafaelhcd@hotmail.com>
2020-01-28 12:25:55 -0300

    commit

--- Desafio 1 - 1 APOS.js
+++ Desafio 1 - 1 APOS.js
@@ -1,1 +1,12 @@
+const nome = 'Rafael'
+const sexo = 'M'
+const idade = 29
+const contribuicao =7
 
+const tcontrib = idade + contribuicao
+
+if (tcontrib >=95 && sexo == 'M' || tcontrib >=85 && sexo == 'F'){
+    console.log(`${nome}, voce pode se aponsentar!`)
+}else {
+    console.log(`${nome}, voce ainda nao pode se aponsentar!`)
+}
