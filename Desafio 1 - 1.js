9be73e4aaedd878fcf443910cf7a4d9dadf922b6
RafaelDalarosa <rafaelhcd@hotmail.com>
2020-01-28 03:06:51 -0300

    commit on master

--- Desafio 1 - 1.js
+++ Desafio 1 - 1.js
@@ -1,9 +1,14 @@
-const usuario = [{
-    nome: 'Rafael',
-    peso: 65,
-    altura: 1.75
-}]
+const nome = 'Rafael'
+const peso = 65
+const altura = 1.75
 
-const imc = 
 
-console.log(imc)
+
+const imc = peso / (altura*altura)
+
+
+if (imc >= 30){
+    console.log('Voce esta acima do Peso.')
+}else{
+    console.log('Voce esta no Peso ideal.')
+}
