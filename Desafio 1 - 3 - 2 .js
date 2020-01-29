7ba4b6c89529ef8d684aae346bcab735061c6938
RafaelDalarosa <rafaelhcd@hotmail.com>
2020-01-29 14:45:56 -0300

    commit

---
+++ Desafio 1 - 3 - 2 .js
@@ -0,0 +1,22 @@
+const usuarios = [{
+    nome: 'Carlos',
+    tecnologias: ['HTML', 'CSS']   
+},
+{
+    nome: 'Jasmine',
+    tecnologias: ['JavaScript', 'CSS']
+},
+{
+    nome: 'Tuane',
+    tecnologias: ['HTML', 'Node.js']
+}]
+
+
+function buscaTech(tech){
+   for (let i = 0; i < tech.length; i++)
+        console.log(`${tech[i].nome} trabalha com ${tech[i].tecnologias}`)
+   }
+
+
+buscaTech(usuarios)
+
