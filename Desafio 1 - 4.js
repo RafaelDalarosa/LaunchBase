c6a5061125c31f75b3583326426a5996188cffd9
RafaelDalarosa <rafaelhcd@hotmail.com>
2020-02-02 04:48:37 -0300

    commit

---
+++ Desafio 1 - 4.js
@@ -0,0 +1,61 @@
+let user = {
+    name: 'Rafael',
+    transaction: [],
+    balance: 0
+
+}
+
+
+function createTransaction(tr,x){
+        if (x >= 0){
+            let y = 0
+            y = user.balance + x
+            user.balance = y
+            console.log(`Seu saldo e de ${user.balance} reais`)
+            tr.transaction.push({
+                type: 'Credit',
+                value: x
+            })
+        }else{
+            if (x <= 0){
+                let y = 0
+                y = user.balance + x
+                user.balance = y
+                console.log(`Seu saldo e de ${user.balance} reais`)
+                tr.transaction.push({
+                    type: 'Debit', 
+                    value: x
+                })
+            } 
+        }
+    }
+
+
+
+function getHigherTransactionByType(x){
+    for (let i = 0; i < x.transaction.length; i++){
+            for (let h = 0; h < x.transaction[i].value; h++){
+                let j = x.transaction.value
+                if (j < x.transaction.value){
+                    x.transaction.value = j
+                    console.log(x) 
+                }
+                    
+            }
+         }
+    
+    }
+    
+    
+getHigherTransactionByType(user)
+
+
+
+
+createTransaction(user, 50)
+createTransaction(user, -20)
+createTransaction(user, 25)
+createTransaction(user, 37)
+
+
+
